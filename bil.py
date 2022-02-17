cc98c571478685a2d5a7f39f9e71749dcaacbb5c
Muna 'Adilah <93821740+munaadilah@users.noreply.github.com>
2022-02-17 21:51:55 +0700

    Add files via upload

---
+++ bil.py
@@ -0,0 +1,9 @@
+def bil(ganjil, genap):
+    a = ganjil + 2
+    b = genap
+    print("bilangan ganjil adalah", a)
+    print("bilangan genap adalah", b)
+    
+bil(1, 4)
+
+
---
+++ luas.py
@@ -0,0 +1,5 @@
+def luas_segitiga(alas, tinggi):
+    luas = (alas * tinggi) / 2
+    print ("Luas segitiga: ", luas)
+
+luas_segitiga(2, 5)
